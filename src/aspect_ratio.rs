816bb65fbc4562e46bea6d58556c919d0106d2f0
João Castel-Branco <136809930+JoaoCB2000@users.noreply.github.com>
2024-01-31 12:37:24 +0000

    last version update

--- src/aspect_ratio.rs
+++ src/aspect_ratio.rs
@@ -1,4 +1,7 @@
-#[derive(Debug, PartialEq, Clone, Copy)]
+use serde_derive::{Serialize, Deserialize};
+
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub enum AspectRatio {
     FourThree,
     SixteenNine,
@@ -6,22 +9,27 @@ pub enum AspectRatio {
     OneOne,
 }
 
+impl AspectRatio {
+    pub fn ratio(&self) -> f64 {
+        match *self {
+            AspectRatio::FourThree => 4.0 / 3.0,
+            AspectRatio::SixteenNine => 16.0 / 9.0,
+            AspectRatio::TwoThree => 2.0 / 3.0,
+            AspectRatio::OneOne => 1.0,
+        }
+    }
+}
+
 
 pub fn get_aspect_ratio(width: f64, height: f64) -> AspectRatio {
     use AspectRatio::*;
     let aspect_ratio = width / height;
 
-    let supported_ratios = [
-        (FourThree, 4.0 / 3.0), 
-        (SixteenNine, 16.0 / 9.0),
-        (TwoThree, 2.0 / 3.0),
-        (OneOne, 1.0)    
-    ];
-
-    supported_ratios
+    [FourThree, SixteenNine, TwoThree, OneOne]
         .iter()
-        .min_by(|&(_, a), &(_, b)| (a - aspect_ratio).abs().partial_cmp(&(b - aspect_ratio).abs()).unwrap())
-        .map(|(name, _)| *name)
-        .expect("Unsupported aspect ratio")
-
+        .min_by(|a, b| {
+            (a.ratio() - aspect_ratio).abs().partial_cmp(&(b.ratio() - aspect_ratio).abs()).unwrap()
+        })
+        .copied()
+        .unwrap()
 }
